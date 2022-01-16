4c0f840b3740d6053c2a84b9e59d15412532fa8f
Brozekm <brozek.m99@gmail.com>
2022-01-16 21:59:09 +0100

    feature: adding snackbar for error responses, creating search for friends

---
+++ src/app/pages/protected/friends/search-friend/search-friend.component.ts
@@ -0,0 +1,57 @@
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatTableDataSource} from "@angular/material/table";
+import {SearchedUserInterface} from "../../../../core/_dataTypes/searchedUser-interface";
+import {BehaviorSubject} from "rxjs";
+import {FriendshipsService} from "../../../../core/_service/protected/friendships.service";
+import {faSearch, faPlus} from "@fortawesome/free-solid-svg-icons";
+
+@Component({
+  selector: 'app-search-friend',
+  templateUrl: './search-friend.component.html',
+  styleUrls: ['./search-friend.component.scss']
+})
+export class SearchFriendComponent implements OnInit, AfterViewInit {
+  faSearch = faSearch;
+  faPlus = faPlus;
+
+  searchForm: FormGroup;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  displayedColumns: string[] = ['user', 'action'];
+  dataSource = new MatTableDataSource<SearchedUserInterface>()
+
+  loaded$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+
+  constructor(private formBuilder: FormBuilder,
+              private friendshipService: FriendshipsService) {
+    this.searchForm = this.formBuilder.group({
+      username: ['', Validators.minLength(3)]
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  ngSubmit() {
+    let user: string = this.searchForm.controls.username.value
+    if (!user || user.length < 3) {
+      this.loaded$.next(false);
+      return;
+    }
+
+    this.friendshipService.getUsersLike(user).subscribe(value => {
+      this.dataSource.data = value;
+      this.loaded$.next(true);
+    });
+  }
+
+  addFriend(element: SearchedUserInterface) {
+
+  }
+}
